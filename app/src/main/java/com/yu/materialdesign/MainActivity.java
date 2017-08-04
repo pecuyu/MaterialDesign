7a8d57a2e6a5743659d2cf1c7cb84c9b77ada75d
unknown <D22436@SZCKT.COM>
2017-08-04 16:03:24 +0800

    add drawer layout

--- app/src/main/java/com/yu/materialdesign/MainActivity.java
+++ app/src/main/java/com/yu/materialdesign/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yu.materialdesign;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -9,13 +12,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.id_tool_bar);
         setSupportActionBar(toolbar);
-
+        drawer = (DrawerLayout) findViewById(R.id.id_layout_drawer);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        }
     }
 
     @Override
@@ -29,13 +38,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.id_menu:
-                Toast.makeText(this,"Menu",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Backup",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_delete:
                 Toast.makeText(this,"Delete",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_comment:
                 Toast.makeText(this,"Comment",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
                 break;
         }
 
